447d12d840f643c9e22f6de0745fa59bb5a8079d
Alex Tsr <ivobakino@gmail.com>
2025-06-28 13:47:53 +0200

    Фикс регистрации /start для aiogram 3.x

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, F
 from aiogram.enums import ParseMode
 from aiogram.types import Message
 from aiogram.fsm.storage.memory import MemoryStorage
@@ -9,12 +9,14 @@ from database import insert_user
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher(storage=MemoryStorage())
 
-@dp.message(F.text.startswith('/start'))
+# ХЭНДЛЕР
 async def start(message: Message):
     await insert_user(message.from_user.id, message.from_user.full_name)
     await message.answer("Привет! Я Goooo — помогу с планами тренировок и питания.")
 
+# ОСНОВНОЙ ЗАПУСК
 async def main():
+    dp.message.register(start, F.text.startswith("/start"))
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
